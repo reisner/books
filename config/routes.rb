eb5053b9785d24a7d22cc05887b50e80fa12ceca
Roman Eisner <ithmatic@gmail.com>
2015-10-20 20:54:56 -0600

    Added static pages

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   #Listing all editions without a book:
   resources :editions, :only => :index
 
+  get "/pages/:page" => "pages#show"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
